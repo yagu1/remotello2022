64c9106318e609f29375cac647d1d1f5c91612aa
hsgucci404 <55542434+hsgucci404@users.noreply.github.com>
2021-11-22 15:52:35 +0900

    Update server.py

--- server/server.py
+++ server/server.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import tello		# tello.pyをインポート
-import time			# time.sleepを使いたいので
-import cv2			# OpenCVを使うため
+import tello		# import tello.py
+import time		# for time.sleep() function
+import cv2			# OpenCV
 import struct
 import redis
 import numpy as np
+import json
 
 def writeRedis(r,a,n):
 	"""Store given Numpy array 'a' in Redis under key 'n'"""
@@ -15,7 +16,6 @@ def writeRedis(r,a,n):
 	encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 50]
 	result, encimg = cv2.imencode('.jpg', a, encode_param)
 	
-	#encoded = shape + a.tobytes()
 	encoded = shape + encimg.tobytes()
 
 	# Store encoded data in Redis
@@ -23,20 +23,32 @@ def writeRedis(r,a,n):
 	return
 
 
-# メイン関数
+# Main Function
 def main():
 	# Redis connection
 	r = redis.Redis(host='os3-380-23015.vs.sakura.ne.jp', port=6379, db=0)
 
-	# Telloクラスを使って，droneというインスタンス(実体)を作る
+	# make instance as "drone" using Tello class
 	drone = tello.Tello( command_timeout=.01 )  
 
-	current_time = time.time()	# 現在時刻の保存変数
-	pre_time = current_time		# 5秒ごとの'command'送信のための時刻変数
+	current_time = time.time()	# current time
+	pre_time = current_time		# for sending 'command' each 5 seconds
 
-	time.sleep(0.5)		# 通信が安定するまでちょっと待つ
-	
-	#Ctrl+cが押されるまでループ
+	time.sleep(0.5)		# wait for stable connection
+
+	# make new OpenCV window for Tarckbar
+	cv2.namedWindow("OpenCV Window")
+
+	# empty function for Trackbar callback
+	def nothing(x):
+		pass
+
+	# make Trackbar
+	cv2.createTrackbar("Battery", "OpenCV Window", 50, 100, nothing)
+	cv2.createTrackbar("Tof", "OpenCV Window", 50, 1000, nothing)
+	cv2.createTrackbar("h", "OpenCV Window", 50, 1000, nothing)
+
+	# loop until Ctrl+c pressed
 	try:
 		while True:
 			cmd = r.get('command')
@@ -48,62 +60,79 @@ def main():
 					r.set('command', '')
 					drone.send_command(cmd)
 
-			# (A)画像取得
-			frame = drone.read()	# 映像を1フレーム取得
-			if frame is None or frame.size == 0:	# 中身がおかしかったら無視
+			# (A)get image
+			frame = drone.read()	# get 1 frame
+			if frame is None or frame.size == 0:		# correct data?
 				continue 
 
-			# (B)ここから画像処理
-			#image = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)		# OpenCV用のカラー並びに変換する
+			# (B)processing
+			#image = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)		# change OpenCV color plane
 			image = frame
-			#small_image = cv2.resize(image, dsize=(320,240) )	# 画像サイズを1/3に変更
-			#small_image = cv2.resize(image, dsize=(480,360) )	# 画像サイズを半分に変更
+			#small_image = cv2.resize(image, dsize=(320,240) )	# change 1/3 size
+			#small_image = cv2.resize(image, dsize=(480,360) )	# change 1/2 size
 
-			# (X)ウィンドウに表示
-			cv2.imshow('OpenCV Window', image)	# ウィンドウに表示するイメージを変えれば色々表示できる
+			# (X)show window
+			cv2.imshow('OpenCV Window', image)
 
-			# (Y)OpenCVウィンドウでキー入力を1ms待つ
+			# show tello info
+			if drone.state is not None:
+				cv2.setTrackbarPos("Battery", "OpenCV Window", drone.state["bat"])
+				cv2.setTrackbarPos("Tof", "OpenCV Window", drone.state["tof"])
+				cv2.setTrackbarPos("h", "OpenCV Window", drone.state["h"])
+
+			# (Y)key input
 			key = cv2.waitKey(1)
-			if key == 27:					# k が27(ESC)だったらwhileループを脱出，プログラム終了
+			if key == 27:					# 27 = ESC key), exit program
 				break
-			elif key == ord('m'):				# モータ回転
+			elif key == ord('m'):				# motor start
 				drone.send_command('motoron')
+			elif key == ord('o'):				# motor stop
+				drone.send_command('motoroff')
 			elif key == ord('t'):
-				drone.takeoff()				# 離陸
+				drone.takeoff()				# takeoff
 			elif key == ord('l'):
-				drone.land()					# 着陸
+				drone.land()					# land
 			elif key == ord('w'):
-				drone.move_forward(0.3)		# 前進
+				drone.move_forward(0.3)		# move forward
 			elif key == ord('s'):
-				drone.move_backward(0.3)		# 後進
+				drone.move_backward(0.3)		# move back
 			elif key == ord('a'):
-				drone.move_left(0.3)			# 左移動
+				drone.move_left(0.3)			# move left
 			elif key == ord('d'):
-				drone.move_right(0.3)			# 右移動
+				drone.move_right(0.3)			# move right
 			elif key == ord('q'):
-				drone.rotate_ccw(20)			# 左旋回
+				drone.rotate_ccw(20)			# turn left
 			elif key == ord('e'):
-				drone.rotate_cw(20)			# 右旋回
+				drone.rotate_cw(20)			# turn right
 			elif key == ord('r'):
-				drone.move_up(0.3)			# 上昇
+				drone.move_up(0.3)			# move up
 			elif key == ord('f'):
-				drone.move_down(0.3)			# 下降
+				drone.move_down(0.3)			# move down
 
-			writeRedis(r, frame, 'image')
+			# (Y) write date to Redis
+			writeRedis(r, frame, 'image')	# write image
 
-			# (Z)5秒おきに'command'を送って、死活チェックを通す
-			current_time = time.time()	# 現在時刻を取得
-			if current_time - pre_time > 5.0 :	# 前回時刻から5秒以上経過しているか？
-				drone.send_command('command')	# 'command'送信
-				pre_time = current_time			# 前回時刻を更新
+			# write tello info and show
+			if drone.state is not None:
+				json_state = json.dumps( drone.state ) 
+				#print(json_state)
+				r.set('state', json_state)
 
-	except( KeyboardInterrupt, SystemExit):    # Ctrl+cが押されたら離脱
-		print( "SIGINTを検知" )
+			# (Z)send 'command' each 5 seconds
+			current_time = time.time()	# current time
+			if current_time - pre_time > 5.0 :	# 5 seconds passed?
+				drone.send_command('command')	# send 'command'
+				pre_time = current_time			# update previous time
 
-	# telloクラスを削除
+
+
+
+	except( KeyboardInterrupt, SystemExit):    # detect Ctrl+c pressed
+		print( "detect SIGINT." )
+
+	# delete tello class
 	del drone
 
 
-# "python main.py"として実行された時だけ動く様にするおまじない処理
-if __name__ == "__main__":		# importされると"__main__"は入らないので，実行かimportかを判断できる．
-	main()    # メイン関数を実行
+if __name__ == "__main__":
+	main()    # call Main Function
